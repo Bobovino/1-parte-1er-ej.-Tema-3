08d30bd139e969ac9fe9fd0955e8195f99513f4c
rodrigo martinez <rodrigomartineztabernero97@gmail.com>
2023-01-31 20:28:02 +0100

    Borrando zips y primer ejercicio Java

---
+++ OpenBootcamp/Java básico/Ejercicio Tema 1/src/Main.java
@@ -0,0 +1,24 @@
+//Para este primer ejercicio tendréis que realizar lo siguiente:
+//        Crea un proyecto de Java desde 0
+//        Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+//        Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+//        Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+
+public class Main {
+    public static void main(String[] args) {
+        int variable1=1;
+        System.out.println(variable1);
+
+        long variable2=2;
+        System.out.println(variable2);
+
+        double variable3=3.1415926;
+        System.out.println(variable3);
+
+        boolean variable4=true;
+        System.out.println(variable4);
+
+        String variable5="Cadena";
+        System.out.println(variable5);
+    }
+}
